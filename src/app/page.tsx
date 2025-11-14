fbf538f3b41697c19f863ff757e7df0a5fd86091
SGChávezJuárez <129635350+Sergardo12@users.noreply.github.com>
2025-11-14 11:33:44 -0500

    chore: 2da redireccion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
   const router = useRouter();
 
   useEffect(() => {
-    router.replace('/auth/');
+    router.replace('/auth/register');
   }, [router]);
 
   return (
